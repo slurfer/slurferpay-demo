e02dcc8d39ba1cc404ad8b059f037c3c5e5d565d
Martin Doušek <dousek.martin@gmail.com>
2025-09-26 13:46:52 +0200

    transactions in booths

--- src/Pages/BoothPage.tsx
+++ src/Pages/BoothPage.tsx
@@ -4,7 +4,8 @@ import config from '../config/booths.json';
 import type { Booth } from '../types/BoothTypes';
 import BackButton from '../components/BackButton';
 import { useState } from 'react';
-import ItemLinkButton from '../components/ItemButton/ItemLinkButton';
+import ItemButton from '../components/ItemButton/ItemButton';
+import ChangeMoneyAmount from './ChangeMoneyAmount';
 
 export default function BoothPage() {
     const [amount, setAmount] = useState(0);
@@ -15,33 +16,44 @@ export default function BoothPage() {
     const booths: Booth[] = config.booths;
     const booth = booths.find((b) => b.link === boothLink);
 
+    function transactionClick(amount: number) {
+        setAmount(amount);
+        setScanning(true);
+    }
+
     if (!booth) {
         alert('Booth not found');
         nav('/');
         return null;
     }
 
-    console.log(booth);
-
-    return (
+    return scanning ? (
+        <ChangeMoneyAmount
+            amount={amount}
+            onClose={() => {
+                setScanning(false);
+                setAmount(0);
+            }}
+        />
+    ) : (
         <Container maxWidth="sm">
             <h1>{booth.name}</h1>
 
             <h2>Nakupuje:</h2>
             {booth.want.map((item) => (
-                <ItemLinkButton
+                <ItemButton
                     key={item.name}
                     name={`${item.name}`}
-                    link={`/`}
+                    onClick={() => transactionClick}
                     type="add"
                 />
             ))}
             <h2>Prodává:</h2>
             {booth.offer.map((item) => (
-                <ItemLinkButton
+                <ItemButton
                     key={item.name}
                     name={`${item.name}`}
-                    link={`/`}
+                    onClick={() => transactionClick(-item.price)}
                     type="subs"
                 />
             ))}
